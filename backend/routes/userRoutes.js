ca785aec9123ea7f2647258e97066b7dd997c237
ME00746 <165137144+ME00746@users.noreply.github.com>
2024-05-03 23:19:45 +0300

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const userController = require('../controllers/userController');
-const express = require('express');
-const router = express.Router();
-
-router.post('/register', userController.createUser);
-router.post('/login', userController.userLogIn);
-
-module.exports = router;
